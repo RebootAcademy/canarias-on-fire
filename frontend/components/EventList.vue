a7569af3dac47c2991861d4d2bb5e611b7067efa
Alber0087 <albertorg0087@gmail.com>
2024-06-19 16:18:17 +0100

    CLOUDINARY: - Image Uploader component created - It loads & uploads an image and returns an URL

--- frontend/components/EventList.vue
+++ frontend/components/EventList.vue
@@ -1,17 +1,11 @@
 <template>
   <div class="h-full flex flex-col items-center p-4">
     <div 
-      v-for="(event, idx) in events"
-      :key="idx"
-      class="w-full flex justify-center p-2"
-      >
-      <EventCard :event="event"/>
+
     </div>
   </div>
 </template>
 
 <script setup>
-
-const { data: events } = await useFetch('api/events/')
 
 </script>
---
+++ frontend/components/ImageUploader.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="flex flex-col gap-2">
+    <div class="flex gap-4 w-2/3">
+      <Input 
+        type="file"
+        @change="handleFileChange"
+      />
+      <Button @click="uploadImage"
+        class="text-xs px-4"
+      >Upload Image</Button>
+    </div>
+    <Label class="text-xs ml-1">Preview</Label>
+    <NuxtImg v-if="eventStore.eventImg" :src="eventStore.eventImg" width="150" />
+  </div>
+</template>
+
+<script setup>
+import { useEventStore } from '../stores/eventStore'
+
+const eventStore = useEventStore()
+const cloudName = 'drs1a2bso'
+
+const handleFileChange = (event) => {
+  eventStore.eventImg = event.target.files[0]
+}
+
+const uploadImage = async () => {
+  if (!eventStore.eventImg) {
+    console.error('No file selected')
+    return
+  }
+
+  try {
+    const formData = new FormData()
+    formData.append("file", eventStore.eventImg)
+    formData.append("upload_preset", "evdhvl07")
+
+    const { data, error } = await useFetch(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, {
+      method: 'POST',
+      body: formData
+    })
+
+    if (error.value) {
+      console.error(error.value)
+      return
+    }
+
+    eventStore.eventImg = data.value.secure_url
+    console.log('Uploaded Image URL:', eventStore.eventImg)
+  } catch (error) {
+    console.error(error)
+  }
+}
+</script>
+
