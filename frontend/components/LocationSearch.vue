ccf40f6462714551ac113fdda54dab36b4f52aa0
Alber0087 <albertorg0087@gmail.com>
2024-06-27 17:07:36 +0100

    Fixed autocomplete and now includes a map

--- frontend/components/LocationSearch.vue
+++ frontend/components/LocationSearch.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-    <!-- <Label for="location" class="font-medium text-xs text-gray-500">Location</Label> -->
     <GMapAutocomplete
       v-model="eventStore.eventLocation"
       ref="autocompleteInput"
-      id="location"
       class="text-sm p-2 border rounded-md w-full"
       placeholder="Enter a location"
       @place_changed="setPlace"
     >
     </GMapAutocomplete>
+    <div class="w-60 h-60">
+      <GMapMap 
+        :center="{lat: 51.093048, lng: 6.842120}"
+        :zoom="7"
+        class="width: 100%; height: 150%;"
+      />
+    </div>
   </div>
 </template>
 
@@ -18,7 +23,7 @@ import { useEventStore } from '../stores/eventStore'
 
 const eventStore = useEventStore()
 
-function setPlace(place) {
+const setPlace = (place) => {
   console.log(place)
   if (place && place.geometry) {
     eventStore.setPlaceDetails(place)
