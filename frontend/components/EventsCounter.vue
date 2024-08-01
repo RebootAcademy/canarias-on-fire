fe783120b0ee48f96ae6c942805beef2dd0ac88e
Alber0087 <albertorg0087@gmail.com>
2024-08-01 15:58:13 +0100

    EventsCounter fixed

--- frontend/components/EventsCounter.vue
+++ frontend/components/EventsCounter.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col items-center p-6">
     <hr class="w-full border-t border-gray-300 mb-4" />
     <div class="flex items-center justify-center w-full px-4">
-      <p class="text-xl font-bold text-center">{{  $t('counter') }} <span class="">{{ eventsCount }}</span></p>
+      <p class="text-xl font-bold text-center">{{  $t('counter') }} <span class="">{{ activeEventsCount }}</span></p>
     </div>
     <hr class="w-full border-t border-gray-300 mt-4" />
   </div>
@@ -10,8 +10,10 @@
 
 <script setup>
 import { useEventStore } from '../stores/eventStore'
-import { storeToRefs } from 'pinia'
 
 const eventStore = useEventStore()
-const { eventsCount } = storeToRefs(eventStore)
+
+const activeEventsCount = computed(() => {
+  return eventStore.events.filter(event => event.status === 'published').length
+})
 </script>
