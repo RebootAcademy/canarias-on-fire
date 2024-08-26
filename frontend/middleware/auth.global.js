dd1331b79419939d1a147fd668f27b61a6492f88
Alber0087 <albertorg0087@gmail.com>
2024-08-26 11:18:49 +0100

    Refactor to manage redirection to onboarding when user logs in

---
+++ frontend/middleware/auth.global.js
@@ -0,0 +1,56 @@
+import { useAuth0 } from '@auth0/auth0-vue'
+import { useUserStore } from '../stores/userStore'
+
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  if (process.client) {
+    const { isAuthenticated, user, getAccessTokenSilently } = useAuth0()
+    const userStore = useUserStore()
+    const config = useRuntimeConfig()
+
+    console.log('Auth middleware executing for route:', to.path)
+
+    if (isAuthenticated.value && user.value && !userStore.isAuthenticated) {
+      console.log('User authenticated, registering and fetching user data')
+      try {
+        const token = await getAccessTokenSilently()
+        const roles = user.value['https://localhost:3000/roles'] || []
+
+        const userData = {
+          email: user.value.email,
+          username: user.value.nickname,
+          role: roles[0] // Asumimos que el primer rol es el principal
+        }
+
+        const response = await fetch(`${config.public.apiBaseUrl}/auth/register`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          },
+          body: JSON.stringify(userData)
+        })
+
+        if (!response.ok) {
+          console.log('Failed to register user:', response.statusText)
+          // Considerar redirigir a una página de error o manejar este caso
+        } else {
+          await userStore.fetchAndSetUser(user.value.email)
+          console.log('User registered and data fetched successfully')
+        }
+      } catch (error) {
+        console.error('Error registering user or fetching data:', error)
+        // Considerar redirigir a una página de error o manejar este caso
+      }
+    }
+
+    // Lógica de redirección basada en roles
+    if (userStore.isAuthenticated) {
+      const role = userStore.userData?.role
+      if (role === 'basic' && to.path !== '/dashboard/onboarding') {
+        return navigateTo('/dashboard/onboarding')
+      } else if (['company', 'musician'].includes(role) && to.path !== '/dashboard') {
+        return navigateTo('/dashboard')
+      }
+    }
+  }
+})
--- frontend/pages/dashboard/index.vue
+++ frontend/pages/dashboard/index.vue
@@ -1,14 +1,12 @@
 <template>
   <div>
-    <Profile v-if="roles.includes('basic')" />
-    <AdminDashboard v-if="roles.includes('admin')" />
-    <CompanyDashboard v-if="roles.includes('company')" />
+    <Profile v-if="userRole === 'basic'" />
+    <AdminDashboard v-if="userRole === 'admin'" />
+    <CompanyDashboard v-if="userRole === 'company'" />
   </div>
 </template>
 
 <script setup>
-import { useAuth0 } from '@auth0/auth0-vue'
-
 definePageMeta({
   layout: 'dashboard'
 })
@@ -17,42 +15,6 @@ useHead({
   title: 'Dashboard'
 })
 
-const { user, isAuthenticated , getAccessTokenSilently} = useAuth0()
-const config = useRuntimeConfig()
 const userStore = useUserStore()
-
-const roles = useState('roles', () => [])
-
-if (isAuthenticated.value && user.value) {
-  roles.value = user.value['https://localhost:3000/roles'] || []
-
-  const userData = {
-    email: user.value.email,
-    username: user.value.nickname,
-    role: roles.value[0] // Asumimos que el primer rol es el principal
-  }
-
-  try {
-    const token = await getAccessTokenSilently()
-    const response = await fetch(`${config.public.apiBaseUrl}/auth/register`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-      },
-      body: JSON.stringify(userData)
-    })
-
-    if (!response.ok) {
-      console.log('Failed to register user:', response.statusText)
-    } else {
-      await userStore.fetchAndSetUser(user.value.email)
-    }
-  } catch (error) {
-    console.log('Error registering user in the database:', error)
-  }
-}
-
-
-
+const userRole = computed(() => userStore.userData?.role)
 </script>
