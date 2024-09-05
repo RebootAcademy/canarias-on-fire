6ec82a54c79ab49ea705553223ee1106c8bdcd42
Adasat <pbonbol@gmail.com>
2024-09-05 08:44:08 +0100

    Persistent Languague selection:
    - nuxt.config.ts:
    Added to i18n, the information about the selected languague.
    
    - LangSelector.vue:
    Keep the selected languague.

--- frontend/components/LangSelector.vue
+++ frontend/components/LangSelector.vue
@@ -15,14 +15,14 @@
           <ul class="p-2">
             <li v-for="lang in languages" :key="lang.code">
               <NavigationMenuLink as-child>
-                <NuxtLink 
-                  :to="switchLocalePath(lang.code)"
+                <button 
+                  @click="setLocale(lang.code)"
                   class="flex items-center w-32 px-3 py-2 text-sm font-normal rounded-md hover:bg-gray-100"
                   :class="locale === lang.code ? 'bg-gray-100' : ''"
                 >
                   <NuxtImg :src="lang.flag" width="20" class="mr-2" />
                   {{ lang.name }}
-                </NuxtLink>
+                </button>
               </NavigationMenuLink>
             </li>
           </ul>
@@ -48,8 +48,7 @@ const currentLocale = ref(locale.value)
 
 const languages = [
   { code: 'es', name: 'Español', flag: 'espana.png' },
-  { code: 'en', name: 'English', flag: 'estados-unidos.png' },
-  { code: 'de', name: 'Deutsch', flag: 'DE.png' }
+  { code: 'en', name: 'English', flag: 'estados-unidos.png' }
 ]
 
 const getFlagSrc = (code) => {
