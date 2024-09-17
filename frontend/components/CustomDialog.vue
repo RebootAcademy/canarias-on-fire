9bc6e21a59c20c73578ff3d9946106751dbb12cf
Adasat <pbonbol@gmail.com>
2024-09-17 17:01:05 +0100

    COMPONENTS: - Added Shadcn's dialog component with his subcomponents:  Dialog.vue / DialogClose.vue / DialogContent.vue / DialogDescription.vue / DialogFooter.vue / DialogTitle.vue / DialogScrollContent / DialogTrigger
     - Created an reusable component 'CustomDialog' which receives a title and a children component (slot)

---
+++ frontend/components/CustomDialog.vue
@@ -0,0 +1,16 @@
+<template>
+  <Dialog>
+    <DialogTrigger as-child>
+      <span class="text-sm italic underline text-blue-400 cursor-pointer hover:text-primary">{{title}}</span>
+    </DialogTrigger>
+    <DialogScrollContent class="xs:max-w-[90%] sm:max-w-[60%] lg:max-w-[90%] bg-black rounded  border-primary">
+      <slot/>
+    </DialogScrollContent>
+  </Dialog>
+</template>
+
+<script setup>
+    const props = defineProps({
+      title: String
+    })
+</script>
