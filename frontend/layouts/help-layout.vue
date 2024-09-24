1fc0917ab8d4d808084de404cd1e6eb20d897bbf
JuanAntonioLeonOjeda <83725404+JuanAntonioLeonOjeda@users.noreply.github.com>
2024-09-24 09:30:00 +0100

    Update help-layout.vue

--- frontend/layouts/help-layout.vue
+++ frontend/layouts/help-layout.vue
@@ -2,22 +2,22 @@
 <div class="flex flex-col min-h-screen bg-black">
     <NavBar />
     <main class="flex-grow w-full">
-      <div class="flex flex-col min-h-screen px-8 py-12">
+        <div class="flex flex-col min-h-screen px-8 py-12">
 
-    <div class="flex items-center flex-col lg:flex-row lg:items-start ">      
-      <!-- Sidebar -->
-      <CustomSidebar :links="helpOptions" :userRole="userRole" />
-  
-      <!-- Main content -->
-        <main class="w-full md:flex-1 p-8">
-          <h1 class="text-4xl text-center font-bold mb-6">{{ $t('helpTitle')}}</h1>
-          <NuxtPage />
-        </main>
-        </div>
-      </div>
+            <div class="flex items-center flex-col lg:flex-row lg:items-start ">      
+              <!-- Sidebar -->
+                <CustomSidebar :links="helpOptions" :userRole="userRole" />
+          
+              <!-- Main content -->
+                <main class="w-full md:flex-1 p-8">
+                    <h1 class="text-4xl text-center font-bold mb-6">{{ $t('helpTitle')}}</h1>
+                    <NuxtPage />
+                </main>
+            </div>
+          </div>
     </main>
     <Footer />
-  </div>
+</div>
 </template>
 
 <script setup>
