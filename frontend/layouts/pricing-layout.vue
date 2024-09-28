9e1ec4bb3f8ca73273742eb825d86376871d1227
Adasat <pbonbol@gmail.com>
2024-09-28 17:17:56 +0100

    Create Payment Section

---
+++ frontend/layouts/pricing-layout.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="flex flex-col min-h-screen bg-background text-secondary">
+    <NavBar />
+    <main class="flex-grow w-full">
+        <div class="flex flex-col min-h-screen px-8 py-6">
+
+            <div class="flex items-center flex-col xl:flex-row lg:items-start ">      
+              <!-- Sidebar -->
+                <CustomSidebar :links="pricingOptions" :userRole="userRole" />
+          
+              <!-- Main content -->
+                <main class="w-full md:flex-1 p-8">
+                    <h1 class="text-4xl text-center font-bold ">{{ $t('plansSection.title')}}</h1>
+                    <NuxtPage />
+                </main>
+            </div>
+          </div>
+    </main>
+    <Footer />
+</div>
+</template>
+
+<script setup>
+const userStore = useUserStore()
+const {t} = useI18n()
+const userRole = computed(() => userStore.userData?.role)
+
+
+const pricingOptions = computed(() => {
+  return [
+    { label: t('plansSection.PromoSubscription'), path: '/pricing/promotions', roles: ['all'] },
+    { label: t('plansSection.eventPayments'), path: '/pricing/events', roles: ['all'] }
+
+  ]
+})
+
+useHead({
+  title: 'FAQ'
+})
+
+</script>
