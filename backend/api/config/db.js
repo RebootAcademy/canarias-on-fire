8be51bf786b2c94862a113c0d8dcb7e11ceebecc
Adasat <pbonbol@gmail.com>
2024-10-03 20:14:51 +0100

    Create DB in Production

--- backend/api/config/db.js
+++ backend/api/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI)
+     await mongoose.connect(process.env.MONGO_URI, {
+       dbName: process.env.DB_NAME,
+     })
     console.log('MongoDB connected')
   } catch (error) {
     console.error('Error connecting to MongoDB', error)
