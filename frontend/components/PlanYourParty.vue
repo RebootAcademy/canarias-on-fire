f4c6b857bfc1e7ca88e8cba479d43cd98442bcbc
JuanAntonioLeonOjeda <jalojr@hotmail.com>
2025-01-16 16:53:38 +0000

    plan your party: cleaning code

--- frontend/components/PlanYourParty.vue
+++ frontend/components/PlanYourParty.vue
@@ -11,42 +11,6 @@
         :user="user"
       />
    </div>
-    <!-- <div
-      v-if="
-        selectCategoryForFilterCompany !== 'bands' &&
-        selectCategoryForFilterCompany !== 'foodtruck'
-      "
-      class="w-full grid justify-items-center items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-2 sm:gap-4 lg:gap-4"
-    >
-      <PromotionCard
-        v-for="promotion in fetchedUsers"
-        :key="promotion._id"
-        :promotion="promotion"
-      />
-    </div>
-    <div
-      v-if="selectCategoryForFilterCompany !== 'bands' && numOfUsers > 9"
-      class="mt-6 w-full justify-center items-center"
-    >
-      <div class="w-1/3">
-        <CustomBtn
-          :title="t('buttons.seeMore')"
-          :action="() => router.push('/promotions')"
-        />
-      </div>
-    </div>
-    <BandList
-      v-if="
-        selectCategoryForFilterCompany &&
-        selectCategoryForFilterCompany === 'bands'
-      "
-    />
-    <FoodtruckList
-      v-if="
-        selectCategoryForFilterCompany &&
-        selectCategoryForFilterCompany === 'foodtruck'
-      "
-    /> -->
   </div>
 </template>
 
@@ -76,7 +40,7 @@ watch(
 
 function filterUsers(category) {
   if (!userStore.users || !Array.isArray(userStore.users)) {
-    userList.value = [] // Reset if there are no users
+    userList.value = []
     return
   }
 
@@ -85,8 +49,6 @@ function filterUsers(category) {
         if (category === 'all' || !category) {
           return user.isActive && user.role !== 'admin'
         }
-        console.log(user.bandName)
-        console.log(user.role)
         if (category === 'bands') {
           return user.role === 'musician'
         }
@@ -95,7 +57,6 @@ function filterUsers(category) {
     .sort((a, b) => getPriority(b) - getPriority(a))
 }
 
-console.log(userList.value)
 onMounted(() => {
   userStore.fetchUsers()
 })
