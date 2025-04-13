5600334c9e0ea11c5c3d1fd6fe233f35f852ca62
Jorge Armas <100132639+JorgeAM1951@users.noreply.github.com>
2025-04-13 13:41:57 +0200

    Added Terms and Conditions

---
+++ frontend/pages/terms-conditions.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="flex flex-row justify-center w-full mt-4">
+      <div class="flex flex-col w-full px-6 md:px-0 md:w-2/3 gap-3">
+        <div class="flex flex-row justify-start text-sm md:text-md">
+          <h1 class="text-3xl text-primary font-bold">
+            {{ $t('footer.terms') }}
+          </h1>
+        </div>
+        <p class="font-bold text-md md:text-lg">Términos y Condiciones de Uso de Evente</p>
+        <p> Fecha de entrada en vigor: 01/04/2025</p>
+        <p class="font-bold text-md md:text-lg">1. Definiciones</p>
+        <ul>
+            <li> Evente: Plataforma online que ofrece información y gestión de eventos en Canarias.</li>
+ <li> Usuario Base: Cualquier persona registrada en Evente con el fin de acceder a la información de eventos, sin funciones adicionales de gestión o pago. </li>
+ <li> Empresa: Entidad registrada en Evente que tiene acceso a herramientas adicionales para promocionar y gestionar eventos, y que puede optar por pagar suscripciónes premium. </li>
+  <li>Suscripción: Plan de pago ofrecido por Evente a las empresas que les permite acceder a funcionalidades y beneficios adicionales en la plataforma. </li>
+        </ul>
+        <p class="font-bold text-md md:text-lg">2. Registro en la plataforma</p>
+        <ul>
+            <li> Para utilizar algunos de nuestros servicios, los usuarios deberán registrarse en la plataforma Evente como Usuario Base o Empresa. Al registrarse, usted acepta proporcionar información precisa y actualizada durante el proceso de registro. </li>
+  <li> Usuario Base: Tendrá acceso a ver y consultar eventos disponibles en Canarias. </li>
+  <li> Empresa: Al registrarse como empresa, tendrá acceso a herramientas para crear, gestionar y promocionar eventos en la plataforma. Además, podrá suscribirse a uno de los planes de pago ofrecidos por Evente para acceder a funciones adicionales. </li>
+
+        </ul>
+        <p class="font-bold text-md md:text-lg">3. Compartir Información</p>
+
+        <ul>
+            <li> Evente ofrece diferentes planes de suscripción para empresas, cada uno con un costo y características específicas. Los planes de suscripción permiten acceder a beneficios adicionales, tales como:</li>
+
+
+<li> Publicidad destacada de eventos.</li>
+
+
+<li> Acceso a herramientas avanzadas de gestión y personalización.</li>
+
+
+<li> Los pagos por suscripciones son recurrentes y se facturan según el plan elegido. Las empresas deberán proporcionar información de pago válida y aceptar los cargos asociados con las suscripciones seleccionadas.</li>
+
+        </ul>
+        <p class="font-bold text-md md:text-lg">4. Condiciones de pago y cancelación</p>
+        <ul>
+            <li> Los pagos de suscripción son procesados mediante métodos de pago habilitados en el sitio web.</li>
+  <li> Las suscripciones se renovarán automáticamente al finalizar el periodo de suscripción vigente, a menos que se cancele antes de la fecha de renovación.</li>
+ <li> La cancelación de la suscripción puede realizarse en cualquier momento, pero no se efectuarán reembolsos por pagos ya realizados.</li>
+        </ul>
+        <p class="font-bold text-md md:text-lg">5. Responsabilidad del Usuario</p>
+        <ul>
+            <li> Usuarios Base: Se comprometen a utilizar Evente de manera legal y ética, respetando los derechos de propiedad intelectual, privacidad y normas aplicables.</li>
+            <li> Empresas: Son responsables de la veracidad de los eventos que publican en la plataforma y deben asegurarse de cumplir con todas las leyes y regulaciones locales relacionadas con sus actividades y los eventos que gestionan.</li>
+        </ul>
+        <p class="font-bold text-md md:text-lg">6. Propiedad intelectual</p>
+        <p>El contenido disponible en la plataforma Evente, incluidos los textos, imágenes, logotipos, gráficos, software y demás materiales, están protegidos por derechos de propiedad intelectual. Queda prohibido el uso no autorizado de dicho contenido sin el consentimiento previo y por escrito de Gerónimo on Fire S.L.</p>
+        <p class="font-bold text-md md:text-lg">7. Modificación de los Términos y Condiciones</p>
+        <p>Gerónimo on Fire S.L. se reserva el derecho de modificar estos Términos y Condiciones en cualquier momento. Las modificaciones serán notificadas a los usuarios mediante la publicación de los nuevos términos en nuestro sitio web. Se recomienda revisar periódicamente los Términos y Condiciones para estar al tanto de cualquier cambio.</p>
+        <p class="font-bold text-md md:text-lg">8. Exoneración de responsabilidad</p>
+        <p>Gerónimo on Fire S.L. no será responsable por daños indirectos, incidentales, especiales o consecuentes, incluidos pero no limitados a la pérdida de ingresos o datos, que resulten del uso o la incapacidad de usar la plataforma Evente.</p>
+        <p class="font-bold text-md md:text-lg">9. Protección de datos personales</p>
+        <p>El tratamiento de los datos personales de los usuarios está sujeto a nuestra Política de Privacidad, que puede consultarse en este enlace. Al registrarse en Evente, usted consiente el tratamiento de sus datos personales de acuerdo con dicha política.</p>
+        <p class="font-bold text-md md:text-lg">10. Legislación aplicable y resolución de conflictos</p>
+        <p>Estos Términos y Condiciones se regirán e interpretarán conforme a la legislación vigente en España. Cualquier disputa relacionada con el uso de Evente será resuelta por los tribunales competentes de Las Palmas de Gran Canaria.</p>
+        <p class="font-bold text-md md:text-lg">11. Contacto</p>
+        <p>Si tiene alguna pregunta sobre estos Términos y Condiciones o desea obtener más información sobre los servicios de Evente, puede contactarnos en:
+            Correo electrónico: info@evente-es.com</p>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  const { t } = useI18n()
+  </script>
+  
