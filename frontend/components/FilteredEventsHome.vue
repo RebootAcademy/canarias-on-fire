10fc9a719794265a514ecdc9eeb43fd32a8bfb61
Jorge Armas <100132639+JorgeAM1951@users.noreply.github.com>
2025-05-06 18:44:43 +0200

    Fixed buttons overlay

--- frontend/components/FilteredEventsHome.vue
+++ frontend/components/FilteredEventsHome.vue
@@ -4,7 +4,7 @@
       v-for="item in options"
       :key="item.value"
       
-      class="flex text-xs md:text-lg items-center text-center  min-w-40 max-w-full md:max-w-64 md:min-h-24 justify-center bg-gray rounded-lg cursor-pointer hover:bg-primary hover:text-background "
+      class="flex text-xs md:text-lg items-center text-center  min-w-40 max-w-full md:max-w-full md:min-h-24 justify-center bg-gray rounded-lg cursor-pointer hover:bg-primary hover:text-background "
       
       @click="selectedOption = item.value"
       :class="{
@@ -46,11 +46,7 @@ const selectedOption = ref('events')
 const options = computed(() => [
   { label: t('buttonsEvents.events'), value: 'events' , image: "/boton_eventos.png"},
   { label: t('buttonsEvents.nightlifePromotion'), value: 'nightlife', image: "/boton_nocturno.png" },
-  {
-    label: t('buttonsEvents.food&DrinksPromotion'),
-    value: 'food&drinks',
-    image: "/boton_bares.png"
-  },
+  { label: t('buttonsEvents.food&DrinksPromotion'), value: 'food&drinks', image: "/boton_bares.png" },
   { label: t('buttonsEvents.kidsPromotion'), value: 'kids', image:"/boton_peques 2.png" },
   { label: t('buttonsEvents.activitiesPromotion'), value: 'activities', image: "/boton_actividades.png" },
   { label: t('buttonsEvents.plansPromotion'), value: 'plans', image: "/boton_escapada.png" },
