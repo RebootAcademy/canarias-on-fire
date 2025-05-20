7ed8298f7b6e17d55bd7a7e2df3676228e98d322
Jorge Armas <100132639+JorgeAM1951@users.noreply.github.com>
2025-05-20 15:41:53 +0200

    Updated social media sharing

--- frontend/pages/events/[id].vue
+++ frontend/pages/events/[id].vue
@@ -409,7 +409,7 @@ const copyToClipboard = async () => {
 const share = () => {
   navigator.share({
     text: "Vente a este evento!",
-    url: "/event/" + eventId
+    url: "/events/" + eventId
   })
 }
 
