ec43f956b4d27ff398527ebcda4f07b4c6642240
Ayo89 <ayose89@gmail.com>
2025-06-11 11:10:04 +0200

    refactor: replace webhook router with brevoWebhook router for email handling

--- backend/api/routes/brevoWebhook.router.js
+++ backend/api/routes/brevoWebhook.router.js
@@ -2,8 +2,6 @@ const router = require('express').Router()
 // const { createEmailEvent } = require('../controllers/webhook.controller')
 const { createEmailEvent } = require('../controllers/brevoWebhook.controller')
 
-
-
-router.post('/send-emails', createEmailEvent)
+router.post('/emails', createEmailEvent)
 
 module.exports = router
