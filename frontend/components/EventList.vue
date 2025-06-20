40c3f2d53ce4a8e8ec7fdec354761babf79a66c2
Ayo89 <ayose89@gmail.com>
2025-06-20 12:05:17 +0200

    Feature: Implementada lógica de priorización y randomizado por nivel

--- frontend/components/EventList.vue
+++ frontend/components/EventList.vue
@@ -29,27 +29,35 @@ const eventStore = useEventStore()
 const userStore = useUserStore()
 const paymentStore = usePaymentStore()
 let category = null
-const { 
-  filteredEvents, 
-  filteredEventsByDate, 
-  filters, 
+const {
+  filteredEvents,
+  filteredEventsByDate,
+  filters,
   selectedEventFilter,
   musicFilter,
   selectedFilterByDate,
   searchQuery,
-  selectedCategories, 
-  categories
-} = storeToRefs(eventStore) 
+  selectedCategories,
+  categories,
+} = storeToRefs(eventStore)
 const props = defineProps({
   type: {
     type: String,
     required: false,
     default: 'kids',
-  }})
+  },
+})
+
 if (props.type === 'kids') {
   //category = categories.value.filter((category) => category.name === 'kids')
   category = categories.value.filter((category) => category.name === 'kids')
   eventStore.toggleCategory(category[0])
+}
+
+const shuffledEvents = ref([])
+
+const shuffleArray = (arr) => {
+  return arr.slice().sort(() => Math.random() - 0.5)
 }
 
 const eventsByDate = computed(() => {
@@ -57,79 +65,110 @@ const eventsByDate = computed(() => {
 })
 
 const noFilterSelected = () => {
-  const {
-    islands,
-    date,
-    startTime,
-    endTime,
-    categories,
-  } = filters.value
+  const { islands, date, startTime, endTime, categories } = filters.value
 
-  return !islands.length && 
-    ! selectedCategories.value.length &&
-    !date && 
-    !startTime && 
-    !endTime && 
-    !categories.length && 
-    selectedEventFilter.value === 'all' && 
+  return (
+    !islands.length &&
+    !selectedCategories.value.length &&
+    !date &&
+    !startTime &&
+    !endTime &&
+    !categories.length &&
+    selectedEventFilter.value === 'all' &&
     musicFilter.value === 'all' &&
     selectedFilterByDate.value === 'all' &&
     !searchQuery.value
+  )
 }
 
 const limitedEvents = computed(() => {
   if (!eventsByDate.value) {
     return []
   }
-  
-  let filterEvents = [...eventsByDate.value]
-    ?.filter(event =>
-      {
-        return (
-          event.status === 'published' &&
-          event.eventType === 'event' && 
-          ((event.userId?.isActive && 
-          event.userId?.isValidated) ||
-          event.userId?.role === 'admin')
-        )
-      }
-    )
 
-  if (userStore.acceptedGeolocation ){
-    filterEvents = filterEvents.filter(event => event.dist?.calculated < eventStore.radioLocation)
+  let filterEvents = [...eventsByDate.value]?.filter((event) => {
+    return (
+      event.status === 'published' &&
+      event.eventType === 'event' &&
+      ((event.userId?.isActive && event.userId?.isValidated) ||
+        event.userId?.role === 'admin')
+    )
+  })
+
+  if (userStore.acceptedGeolocation) {
+    filterEvents = filterEvents.filter(
+      (event) => event.dist?.calculated < eventStore.radioLocation
+    )
   }
 
   if (eventStore.musicFilter !== 'all') {
-    filterEvents = filterEvents.filter(event => event.musicType === eventStore.musicFilter)
+    filterEvents = filterEvents.filter(
+      (event) => event.musicType === eventStore.musicFilter
+    )
   }
 
-  const eventsWithRandomOrder = filterEvents.map(event => ({
+  const eventsWithRandomOrder = filterEvents.map((event) => ({
     ...event,
     randomOrder: Math.random().toFixed(2),
   }))
-    
-  return eventsWithRandomOrder
-    .sort((a, b) => {
-      // Primero compara las prioridades
-      const priorityA = getEventPriority(a) || undefined
-      const priorityB = getEventPriority(b) || undefined
 
-      if ((!priorityA && priorityB) || (priorityA > priorityB)) {
-        return 1 // Coloca los eventos sin "paymentId" al final
-      }
-      if ((priorityA && !priorityB) || (priorityA < priorityB)) {
-        return -1 // Coloca los eventos con "paymentId" al principio
-      }
+  return eventsWithRandomOrder.sort((a, b) => {
+    // Primero compara las prioridades
+    const priorityA = getEventPriority(a) || undefined
+    const priorityB = getEventPriority(b) || undefined
 
-      if (eventStore.selectedFilterByDate !== 'all') {
-        return compareDates(a.eventDate, b.eventDate)
-      } else {
-        return a.randomOrder - b.randomOrder
-      }
-    })
+    if ((!priorityA && priorityB) || priorityA > priorityB) {
+      return 1 // Coloca los eventos sin "paymentId" al final
+    }
+    if ((priorityA && !priorityB) || priorityA < priorityB) {
+      return -1 // Coloca los eventos con "paymentId" al principio
+    }
+
+    if (selectedFilterByDate.value !== 'all') {
+      return compareDates(a.eventDate, b.eventDate)
+    } else {
+      return a.randomOrder - b.randomOrder
+    }
+  })
 })
 
-const displayEvents = computed(() => noFilterSelected() ? limitedEvents.value.slice(0,9): limitedEvents.value)
+
+// Random Order By priority Event
+watch(
+  limitedEvents,
+  (newList) => {
+    const priorities = {
+      3: [],
+      2: [],
+      1: [],
+    }
+    const noPriority = []
+
+    newList.forEach((event) => {
+      const priority = getEventPriority(event)
+      if (priority && priorities[priority]) {
+        priorities[priority].push(event)
+      } else {
+        noPriority.push(event)
+      }
+    })
+
+    shuffledEvents.value = [
+      ...shuffleArray(priorities[3]),
+      ...shuffleArray(priorities[2]),
+      ...shuffleArray(priorities[1]),
+      ...noPriority,
+    ]
+  },
+  { immediate: true }
+)
+
+const displayEvents = computed(() => {
+  if (noFilterSelected()) {
+    return shuffledEvents.value.slice(0, 9)
+  }
+  return shuffledEvents.value
+})
 
 function getEventPriority(event) {
   const paymentId = event.type === 'event' ? event.payment?._id : event.payment
