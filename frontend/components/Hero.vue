30fe756bfd9fd78966ea4602cd2a017a4e0fb532
Ayo89 <ayose89@gmail.com>
2025-06-23 11:29:55 +0200

    feat: Update FilteredEventsHomeMobile component layout and options for better user experience feat: Adjust Hero component image layout for improved responsiveness feat: Add 'promotions' key to English and Spanish event localization files feat: Add caret-down icon for dropdown functionality in FilteredEventsHomeMobile

--- frontend/components/Hero.vue
+++ frontend/components/Hero.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="flex justify-center items-center bg-background p-8 w-full">
-    <NuxtImg src="/banner.png" alt="Hero image" class="w-full object-contain object-center h-fit"/>
+    <NuxtImg src="/banner.png" layout="responsive" alt="Hero image" class="w-full  h-[22rem]"/>
   </div>
 </template>
-
