f4be848d9628614b7f5f4a008104e9300a1344c5
Ayo89 <ayose89@gmail.com>
2025-07-03 15:03:57 +0200

    feat(scraper): enhance Puppeteer scraping capabilities with stealth and user-agent customization
    - Updated Puppeteer to use puppeteer-extra and added stealth plugin for improved scraping.
    - Implemented user-agent rotation using the user-agents package.
    - Enhanced browser launch options for better performance and compatibility.
    - Added a new test script for scraping various government websites in the Canary Islands.
    - Updated package.json and package-lock.json to include new dependencies: puppeteer-extra, puppeteer-extra-plugin-stealth, and user-agents.

---
+++ backend/testScraper.js
@@ -0,0 +1,30 @@
+
+require('dotenv').config();
+
+const dbConnect = require('./api/config/db')
+const scrapperGobiernoExpo = require('./api/scraping/gobiernoCanariasExpo');
+const scrapeAytoTenerife = require('./api/scraping/ayuntamientoTenerife.js')
+const scrapeAytoLasPalmas = require('./api/scraping/ayuntamientoLasPalmas.js')
+const scrapeCabildoGranCanaria = require('./api/scraping/cabildoLasPalmas');
+const scrapeGobCanarias = require('./api/scraping/gobiernoCanarias');
+const scrapeTeaTenerife = require('./api/scraping/teaTenerife');
+async function main() {
+    try {
+        await dbConnect()
+        await scrapeCabildoGranCanaria()
+        //await scrapeGobCanarias()
+        //await scrapperGobiernoExpo()
+        //await scrapeAytoLasPalmas()
+        //await scrapeAytoTenerife()
+        //await scrapeTeaTenerife()
+        console.log('Scraping completed successfully.')
+    } catch (error) {
+        console.error('Error during scraping:', error)
+    }
+}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error('Error in main function:', error)
+    process.exit(1)
+  })
